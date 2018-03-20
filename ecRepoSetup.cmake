a9670ff6ace8d0311f4082241c225ccf876dc223
Piyoosh Mukhija <piyoosh@energid.com>
2018-03-20 14:52:52 -0400

    ENHANCEMENT: Build system compatibility updates for building the repository inside Energid's build system

--- ecRepoSetup.cmake
+++ ecRepoSetup.cmake
@@ -4,3 +4,7 @@ ecGetScriptDir(inst)
 if (NOT EC_EMBEDDED)
     ecBundle(render_OSS)
 endif (NOT EC_EMBEDDED)
+
+if(";${EC_REPOSITORIES};" MATCHES ";osgqtquick;")
+   set(OSGQTQUICK_ACTIN_BUILD TRUE INTERNAL)
+endif(";${EC_REPOSITORIES};" MATCHES ";osgqtquick;")
