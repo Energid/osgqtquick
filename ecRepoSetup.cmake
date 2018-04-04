59ce4008a48dba195b3820c228ec9f7d5226a955
Piyoosh Mukhija <piyoosh@energid.com>
2018-04-04 16:44:00 -0400

    ENHANCEMENT: Removed EC_EMBEDDED checkk for render_OSS setup

--- ecRepoSetup.cmake
+++ ecRepoSetup.cmake
@@ -1,9 +1,8 @@
 # Get the directory where this config file resides
 ecGetScriptDir(inst)
 
-if (NOT EC_EMBEDDED)
-    ecBundle(render_OSS)
-endif (NOT EC_EMBEDDED)
+# We assume that osgqtquick repository is being built only for non-embedded builds
+ecBundle(render_OSS)
 
 if(";${EC_REPOSITORIES};" MATCHES ";osgqtquick;")
    set(OSGQTQUICK_ACTIN_BUILD TRUE CACHE INTERNAL "Flag indicating whether OsgQtQuick is being built as Actin dependency")
