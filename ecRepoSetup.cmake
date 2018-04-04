1fb6c1e1b5a31c87876b5cb37c2b0577fc9e5ffb
Piyoosh Mukhija <piyoosh@energid.com>
2018-04-04 17:18:21 -0400

    ENHANCEMENT: Added packaging of license file for Actin installers and removed unnecessary check for osgqtquick repository being included in EC_REPOSITORIES.

--- ecRepoSetup.cmake
+++ ecRepoSetup.cmake
@@ -3,7 +3,5 @@ ecGetScriptDir(inst)
 
 # We assume that osgqtquick repository is being built only for non-embedded builds
 ecBundle(render_OSS)
-
-if(";${EC_REPOSITORIES};" MATCHES ";osgqtquick;")
-   set(OSGQTQUICK_ACTIN_BUILD TRUE CACHE INTERNAL "Flag indicating whether OsgQtQuick is being built as Actin dependency")
-endif(";${EC_REPOSITORIES};" MATCHES ";osgqtquick;")
+set(OSGQTQUICK_ACTIN_BUILD TRUE CACHE INTERNAL "Flag indicating whether OsgQtQuick is being built as Actin dependency")
+ecPackageFiles(bundles-render_OSS LICENSE)
